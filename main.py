579c9052b805b83d4488b2c69a95ad1761afafa7
João Victor <jvpsierra@gmail.com>
2020-01-06 13:14:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,46 @@
 Created on Fri Jan  3 11:57:59 2020
 
 @author: João Victor Sierra
+@description: an Pomodoro Timer written in Python
 """
 
 import tkinter as tk
 import time
 from winsound import *
+import configparser
 
-class Application:
-    timeStr = "25:00"
+def readConfigFile():
+    config = configparser.ConfigParser()
+    
+    config.read("config.ini")
+    
+    values = (config["DEFAULT"]["PomodoroTime"], config["DEFAULT"]["ShortBreakTime"],
+              config["DEFAULT"]["LongBreakTime"]) 
+    
+    return values
+
+class MainWindow:
+    timeStr = ""
+    
+    pomodoroValue = ""
+    shortBreakValue = ""
+    longBreakValue = ""
+    
     running = False
     counter = 3
+    
+    def setTimeOptions(self):
+        self.pomodoroValue, self.shortBreakValue, self.longBreakValue = readConfigFile()
+    
+        self.pomodoroValue += ":00"
+        self.shortBreakValue += ":00"
+        self.longBreakValue += ":00"
+
+        self.pomodoroValue = str(time.strftime("%M:%S", time.strptime(self.pomodoroValue, "%M:%S")))
+        self.shortBreakValue = str(time.strftime("%M:%S", time.strptime(self.shortBreakValue, "%M:%S")))
+        self.longBreakValue = str(time.strftime("%M:%S", time.strptime(self.longBreakValue, "%M:%S")))
+
+        self.changeCronometerLabelText(self.pomodoroValue)
     
     def __init__(self, master=None):  
         self.optionsButtonsContainer = tk.Frame(master)
@@ -26,6 +56,9 @@ class Application:
         
         self.buttonsContainer = tk.Frame(master)
         self.buttonsContainer.pack()
+        
+        self.footerContainer = tk.Frame(master)
+        self.footerContainer.pack()
 
         self.btnPomodoro = tk.Button(self.optionsButtonsContainer)
         self.btnPomodoro["text"] = "Pomodoro" 
@@ -69,12 +102,18 @@ class Application:
         self.btnReset["bg"] = "lightgray"
         self.btnReset.pack(side=tk.LEFT)
     
-        self.changeCronometerLabelText("25:00")
+        self.btnOptions = tk.Button(self.footerContainer)
+        self.btnOptions["text"] = "Options"
+        self.btnOptions["command"] = lambda: self.evtBtnOptions()
+        self.btnOptions.pack()
+    
+        self.setTimeOptions()
     def get_sec(self, time_str):
         m, s = time_str.split(':')
         return int(m) * 60 + int(s)
     
     def changeCronometerLabelText(self, text): 
+        self.timeStr = text
         self.labelCronometer["text"] = text   
         
     def count(self):
@@ -86,7 +125,7 @@ class Application:
             self.counter -= 1
             
         if self.counter == 0:
-            PlaySound(r'resources\Alarm Sound.wav', SND_FILENAME)
+            PlaySound('Alarm Sound.wav', SND_FILENAME)
             self.running = False
         
     def countTime(self):    
@@ -109,30 +148,82 @@ class Application:
         print(self.running)
         
     def evtBtnPomodoro(self):
-        self.timeStr = "25:00"
-        self.changeCronometerLabelText(self.timeStr)
-        
-        if self.running:
-            self.evtBtnStart()
+        self.changeCronometerLabelText(self.pomodoroValue)
         
     def evtBtnShortBreak(self):
-        self.timeStr = "05:00"
-        self.changeCronometerLabelText(self.timeStr)
-        
-        if self.running:
-            self.evtBtnStart()
+        self.changeCronometerLabelText(self.shortBreakValue)
         
     def evtBtnLongBreak(self):
-        self.timeStr = "10:00"
-        self.changeCronometerLabelText(self.timeStr)
-    
-        if self.running:
-            self.evtBtnStart()
-    
+        self.changeCronometerLabelText(self.longBreakValue)
+
+    def evtBtnOptions(self):
+        rootOpt = tk.Toplevel()
+        ConfigurationWindow(rootOpt)
+        rootOpt.title("Options")
+        rootOpt.resizable(0, 0)
+        rootOpt.iconbitmap(r'C:\Users\Pichau\.spyder-py3\Pomodoro\resources\icons8-tomato-48.png')
+        rootOpt.geometry("250x150")
+        rootOpt.mainloop()
+
+class ConfigurationWindow:
+    def __init__(self, master=None):  
+            self.entriesContainer = tk.Frame(master)
+            self.entriesContainer.pack()
+            
+            self.lblPomodoro = tk.Label(self.entriesContainer)
+            self.lblPomodoro["text"] = "Pomodoro Time"
+            self.lblPomodoro.pack()
+            
+            self.entryPomodoro = tk.Entry(self.entriesContainer)
+            self.entryPomodoro.pack()
+            
+            self.lblShortBreak = tk.Label(self.entriesContainer)
+            self.lblShortBreak["text"] = "Short Break Time"
+            self.lblShortBreak.pack()
+            
+            self.entryShortBreak = tk.Entry(self.entriesContainer)
+            self.entryShortBreak.pack()
+            
+            self.lblLongBreak = tk.Label(self.entriesContainer)
+            self.lblLongBreak["text"] = "Long Break Time"
+            self.lblLongBreak.pack()
+            
+            self.entryLongBreak = tk.Entry(self.entriesContainer)
+            self.entryLongBreak.pack()
+            
+            self.btnSave = tk.Button(self.entriesContainer)
+            self.btnSave["text"] = "Save Options"
+            self.btnSave["bg"] = "lightblue"
+            self.btnSave["command"] = lambda: self.saveConfiguration()
+            self.btnSave.pack()
+         
+            self.setEntryValues()
+            
+    def setEntryValues(self):
+        pomodoro, short, long = readConfigFile()        
+            
+        self.entryPomodoro.delete(0, tk.END)
+        self.entryPomodoro.insert(0, pomodoro)
+        
+        self.entryShortBreak.delete(0, tk.END)
+        self.entryShortBreak.insert(0, short)
+        
+        self.entryLongBreak.delete(0, tk.END)
+        self.entryLongBreak.insert(0, long)
+        
+    def saveConfiguration(self):
+        config = configparser.ConfigParser()
+        
+        config["DEFAULT"] = {"PomodoroTime": self.entryPomodoro.get(),
+                             "ShortBreakTime": self.entryShortBreak.get(),
+                             "LongBreakTime": self.entryLongBreak.get()}
+        
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+
 root = tk.Tk()
-root.geometry("300x185")
 root.title("Pomodoro Timer")
 root.resizable(0, 0)
-root.iconbitmap(r'resources\icons8-tomato-48.png')
-Application(root)
+root.iconbitmap(r'C:\Users\Pichau\.spyder-py3\Pomodoro\resources\icons8-tomato-48.png')
+MainWindow(root)
 root.mainloop()
